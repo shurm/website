881d7750f18c53b4be3add70d82514e030d166f8
mas868 <mas868@scarletmail.rutgers.edu>
2018-08-22 01:25:18 -0400

    added header and footer php files

---
+++ assets/templates/footer.php
@@ -0,0 +1,56 @@
+</div>	<!-- /container -->
+
+
+
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 widget">
+				<h3 class="widget-title">Contact</h3>
+				<div class="widget-body">
+					<p>7324924241<br>
+						<a href="mailto:#">michaelshur28@gmail.com</a><br>
+						
+					</p>	
+				</div>
+			</div>
+
+			<div class="col-md-3 widget">
+				<h3 class="widget-title">Follow me</h3>
+				<div class="widget-body">
+					<p class="follow-me-icons">
+						
+						<a href=""><i class="fa fa-linkedin fa-2"></i></a>
+						<a href=""><i class="fa fa-github fa-2"></i></a>
+						<!--put linkin here -->
+						
+					</p>
+				</div>
+			</div>
+
+			<div class="col-md-6 widget">
+				<div class="widget-body">
+					<p class="text-right">
+						Copyright &copy; 2014, Michael Shur<br> 
+						Design: <a href="http://www.gettemplate.com" rel="designer">Initio by GetTemplate</a> </p>
+				</div>
+			</div>
+
+		</div> <!-- /row of widgets -->
+	</div>
+</footer>
+
+
+
+
+
+<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
+<!--Necessary-->
+<script src="assets/js/main.js"></script>
+
+
+</body>
+</html>
---
+++ assets/templates/header.php
@@ -0,0 +1,81 @@
+<?php
+if(count(get_included_files()) ==1) exit("Direct access not permitted.");
+?>	
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+
+		<meta charset="utf-8">
+		<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+		
+		<?php
+			echo "<title>$title</title>";
+
+		?>
+		<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+		
+		<!-- Bootstrap -->
+		<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+		<!-- Icons -->
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		<!-- Fonts -->
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
+		<!-- Custom styles -->
+		
+		<link rel="stylesheet" type="text/css" href="assets/css/util.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	
+		<link rel="stylesheet" href="assets/css/styles.css">
+
+		<!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
+	</head>
+	<body class="home">
+
+		<header id="header">
+			<div id="head" style = "background: #f4f4f4 url(assets/images/bg_head6.jpg) top center;
+		  background-size: 100% 1000px;" class="parallax" parallax-speed="2">
+				<h1 id="logo" class="text-center">
+					<img class="img-circle" src="assets/images/welcome.jpg" alt="">
+					<span class="title">Michael Shur</span>
+					<span class="tagline">Computer Programmer and Tutor<br>
+						<a href="">michaelshur28@gmail.com</a></span>
+				</h1>
+				
+			</div>
+			
+
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span>
+							<span class="icon-bar"></span> <span class="icon-bar"></span> 
+						</button>
+					</div>
+					
+					<div class="navbar-collapse collapse" id="myNavbar">
+						
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="projects.html#Games">Games</a></li>
+									<li><a href="projects.html#Games">Source Code</a></li>
+								</ul>
+							</li>
+							<li><a href="blog.html">Blog</a></li>
+						</ul>
+					
+					</div><!--/.nav-collapse -->			
+				</div>	
+			</nav>
+		</header>
+
+
+		<div class="container"> 
